35bf8d780f1bbe024271aca46391333dd1adbc37
Kameron Arceneaux <karce13@lsu.edu>
2025-01-17 13:53:58 -0600

    Lecture 21: Seeding data in the database

--- Data/HoteListingDbContext.cs
+++ Data/HoteListingDbContext.cs
@@ -13,5 +13,61 @@ namespace HotelListing.API.Data
         public DbSet<Hotel> Hotels { get; set; }
         public DbSet<Country> Coutries { get; set; }
 
+
+        /**
+         * Automatically adds data to the database
+         */
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+            modelBuilder.Entity<Country>().HasData(
+                    new Country
+                    {
+                        Id = 1,
+                        Name = "Jamaica",
+                        ShortName = "JM"
+                    },
+                    new Country
+                    {
+                        Id = 2,
+                        Name = "Bahamas",
+                        ShortName = "BS"
+                    },
+                    new Country
+                    {
+                        Id = 3,
+                        Name = "Cayman Island",
+                        ShortName = "CI"
+                    }
+            );
+
+            modelBuilder.Entity<Hotel>().HasData(
+                new Hotel
+                {
+                    Id = 1,
+                    Name = "Sandals Resort and Spa",
+                    Address = "Negril",
+                    CountryId = 1,
+                    Rating = 4.5
+                },
+                new Hotel
+                {
+                    Id = 2,
+                    Name = "Comfort Suites",
+                    Address = "Negril",
+                    CountryId = 3,
+                    Rating = 4.5
+                },
+                new Hotel
+                {
+                    Id = 3,
+                    Name = "Grand Palldium",
+                    Address = "Nassua",
+                    CountryId = 2,
+                    Rating = 4
+                }
+            );
+
+        }
     }
 }
